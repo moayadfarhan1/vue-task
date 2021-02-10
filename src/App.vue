efc6fd06c53c53f7716a95e0d19c16a5a516f2e2
moayadfarhan <moayad.farhan@mawdoo3.com>
2021-02-10 17:44:22 +0200

    top section

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,109 @@
 <template>
   <div id="app">
-    <div class="flex-parent">
-      <div
-        class="flex-1 flex-parent flex-direction-column justify-content-center"
-      >
-        1
-      </div>
-      <div
-        class="flex-2 flex-parent flex-direction-column justify-content-center"
-      >
-        2
-      </div>
-      <div
-        class="flex-3 flex-parent flex-direction-column justify-content-center"
-      >
-        3
-      </div>
-      <div class="flex-4 flex-parent align-items-center">
-        <div class="flex-parent flex-direction-column">
-          <div class="flex-1">row 1</div>
-          <div class="flex-1">row 2</div>
-          <div class="flex-1">row 3</div>
+    <section class="top-section bg-image">
+      <div class="container container-sm container-md container-lg position-relative top-section-container">
+        <!-- start Header -->
+        <header class="header">
+          <!-- Logo -->
+          <a href="http://" target="_blank" class="logo">
+            <img src="@/assets/images/NT.svg" alt="logo" />
+          </a>
+          <!-- End Logo -->
+          <!-- Action -->
+          <div class="d-flex align-center">
+            <a href="/" target="_blank" rel="noopener noreferrer">
+              <img
+                src="@/assets/images/search-solid.svg"
+                class="search-icon"
+                alt="Search Logo"
+              />
+            </a>
+            <button class="btn">Menu</button>
+          </div>
+          <!-- End Action -->
+        </header>
+        <!-- End Header -->
+        <div class="d-flex flex-column justify-center about-trips">
+          <h1 class="title">Night Trips</h1>
+          <h3 class="sub-title">WE GOT TRIPS FOR THE TRIPPSTER IN YOU</h3>
+          <p>
+            Neque, eros commodo, nascetur ullamcorper vitae. Tincidunt ut
+            venenatis, volutpat lorem ut faucibus mauris, quisque. Integer
+            gravida sed quis congue. Vel risus, arcu a viverra leo id pulvinar
+            ultricies. Enim in in in pulvinar nulla sollicitudin. Ullamcorper.
+          </p>
         </div>
+          <div class="social-links">
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+              <img src="@/assets/images/twitter.svg" alt="twitter" srcset=""/>
+            </a>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+              <img width="20px" src="@/assets/images/instagram.svg" alt="twitter" srcset=""/>
+            </a>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+              <img src="@/assets/images/facebook.svg" alt="twitter" srcset=""/>
+            </a>
+          </div>
+          <div class="scroll-container">
+            <span class="scroll-text">scroll</span>
+          </div>
       </div>
-    </div>
+      <div class="divider"></div>
+    </section>
+    <section class="bottom-section bg-bottom">
+      <div class="container container-sm container-md container-lg">
+        asdasdsa
+      </div>
+    </section>
   </div>
 </template>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {};
+  },
+  components: {},
+  methods() {},
+};
+</script>
 
 <style lang="scss">
-.flex-parent {
-  // Add this class to create a flexbox parent
-  display: flex;
-  &.flex-direction-column {
-    // This changes the layout direction from the default horizontal to vertical
-    flex-direction: column;
-  }
-  &.justify-content-center {
-    // This centers the parent's children along the main axis (horizontal by default, vertical if the parent has the  flex-direction-column class)
-    justify-content: center;
-  }
-  &.align-items-center {
-    // This centers the parent's children along the cross axis
-    justify-content: center;
-  }
-  &.flex-wrap-wrap {
-    // This allows the children to wrap as needed
-    flex-wrap: wrap;
-  }
-  @for $i from 1 through 12 {
-    // Here we loop through numbers 1 to 12, generating flex child classes for each number, eg. .flex-1, .flex-2 etc.
-    .flex-#{$i} {
-      flex: $i;
-    }
-  }
+@import "./assets/scss/container";
+@import "./assets/scss/common";
+
+html,
+body {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: "Larsseit";
+  color: #fff;
+  background-color: #011627;
+}
+@font-face {
+  font-family: "Larsseit";
+  src: url("./assets/fonts/Larsseit.eot");
+  src: url("./assets/fonts/Larsseit.eot?#iefix") format("embedded-opentype"),
+    url("./assets/fonts/Larsseit.woff2") format("woff2"),
+    url("./assets/fonts/Larsseit.woff") format("woff"),
+    url("./assets/fonts/Larsseit.ttf") format("truetype");
+  font-weight: 400;
+  font-style: normal;
+  font-display: swap;
+}
+@font-face {
+  font-family: "Noe Display";
+  src: url("./assets/fonts/NoeDisplay-Bold.eot");
+  src: url("./assets/fonts/NoeDisplay-Bold.eot?#iefix")
+      format("embedded-opentype"),
+    url("./assets/fonts/NoeDisplay-Bold.woff2") format("woff2"),
+    url("./assets/fonts/NoeDisplay-Bold.woff") format("woff"),
+    url("./assets/fonts/NoeDisplay-Bold.ttf") format("truetype");
+  font-weight: bold;
+  font-style: normal;
+  font-display: swap;
 }
 </style>
