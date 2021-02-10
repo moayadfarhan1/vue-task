671bd2471819d459d7f0d9017417b34f8f1e0186
moayadfarhan <moayad.farhan@mawdoo3.com>
2021-02-10 10:59:46 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,56 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="flex-parent">
+      <div
+        class="flex-1 flex-parent flex-direction-column justify-content-center"
+      >
+        1
+      </div>
+      <div
+        class="flex-2 flex-parent flex-direction-column justify-content-center"
+      >
+        2
+      </div>
+      <div
+        class="flex-3 flex-parent flex-direction-column justify-content-center"
+      >
+        3
+      </div>
+      <div class="flex-4 flex-parent align-items-center">
+        <div class="flex-parent flex-direction-column">
+          <div class="flex-1">row 1</div>
+          <div class="flex-1">row 2</div>
+          <div class="flex-1">row 3</div>
+        </div>
+      </div>
     </div>
-    <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+.flex-parent {
+  // Add this class to create a flexbox parent
+  display: flex;
+  &.flex-direction-column {
+    // This changes the layout direction from the default horizontal to vertical
+    flex-direction: column;
+  }
+  &.justify-content-center {
+    // This centers the parent's children along the main axis (horizontal by default, vertical if the parent has the  flex-direction-column class)
+    justify-content: center;
+  }
+  &.align-items-center {
+    // This centers the parent's children along the cross axis
+    justify-content: center;
+  }
+  &.flex-wrap-wrap {
+    // This allows the children to wrap as needed
+    flex-wrap: wrap;
+  }
+  @for $i from 1 through 12 {
+    // Here we loop through numbers 1 to 12, generating flex child classes for each number, eg. .flex-1, .flex-2 etc.
+    .flex-#{$i} {
+      flex: $i;
     }
   }
 }
